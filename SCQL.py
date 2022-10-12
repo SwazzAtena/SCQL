01f295330782408c8bdb9561edf7a99c7ce745e3
CedkanTurkyafes <68772806+omertugrulbayram@users.noreply.github.com>
2022-10-12 18:42:32 +0300

    Create SCQL.py

---
+++ SCQL.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3 
+
+import os
+
+os.system("pkg install toilet")
+os.system("clear")
+os.system("toilet -f bigmono12 SCQL -F gay")
+print("""
+
+1)İnformation About The Target Site(Site Hakkında Bilgi Toplama)
+2)Fast Port Scanner(Hızlı Port Tarama)
+3)Version İnformation(Versiyon Bilgisi)
+4)SQL Dork Scanner(SQL Açık Tarama)
+
+""")
+
+islemno= input("Select The Number :")
+
+if islemno=="1":
+  hedefip= input("Enter Target Site: ")
+  os.system("whois "+hedefip)
+  elif islemno=="2"
+  hedefip= input("Enter Targer The Site: ")
+  os.system("nmap "+hedefip)
+  elif islemno=="3"
+  hedefip= input("Enter Target Site: ")
+  os.system("nmap -sV "+hedefip)
+  elif islemno=="4"
+  hedefip= input("Enter Dork: ")
+  os.system("sqlmap -g "+hedefip)
+  
